afc74779d86428927b3f02f62d3dfa0c49578f30
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2023-01-11 18:42:12 -0300

    Update connect_server.c

--- connect_server.c
+++ connect_server.c
@@ -9,28 +9,28 @@ int main(int argc, char* argv[])
     SOCKET skt;
 	
     struct sockaddr_in sin;
-    char buffer[] = "A quick brown fox jumps over the lazy dog!";
-	char ip[] = "142.251.129.206";
+    char buffer[] = "The quick brown fox jumps over the lazy dog!";
+    char ip[] = "142.251.129.206"; //Google's IP
     
     //Initializates winsock dll
     int init = WSAStartup(MAKEWORD(2, 2), &wsa);
     if (init != 0) {
-        printf("\nWSAStartup failed: %d\n", init);
+        printf("\nWinsock's dll failed: %d\n", init);
         return 1;
     } else {
-		printf("\nWinsock started.\n");
-	}
+		printf("\nWinsock's dll started.\n");
+    }
 
-    //Create socket
+    //Creates socket
     skt = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (skt == INVALID_SOCKET) {
         printf("\nSocket failed: %d\n", skt);
         return 1;
     } else {
 		printf("Socket created.\n");
-	}
+    }
 
-    //Fill sockaddr_in struct
+    //Fills sockaddr_in struct
     sin.sin_addr.s_addr = inet_addr(ip); //Google IP
     sin.sin_family = AF_INET; //IPv4
     sin.sin_port = htons(PORT);
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
         return 1;
     } else {
 		printf("Connected to %s.\n", ip);
-	}	
+    }	
 
     //Sends the message
     int sendmsg = send(skt, buffer, sizeof(buffer), 0);
@@ -51,16 +51,16 @@ int main(int argc, char* argv[])
         return 1;
     } else {
 		printf("Message sent.\n");
-	}
+    }
 	
-	//Shutdown the socket
-	int end = shutdown(skt, SD_SEND);
-	if (end == SOCKET_ERROR) {
-		printf("\nShutdown failed: %d\n", end);
-		return 1;
-	} else {
-		printf("Socket Desligado.\n");
-	}
+    //Shutdown the connection
+    int end = shutdown(skt, SD_SEND);
+    if (end == SOCKET_ERROR) {
+	    printf("\nShutdown failed: %d\n", end);
+	    return 1;
+    } else {
+	    printf("Connection off.\n");
+    }
 
     //Close the socket and terminates winsock dll
     closesocket(skt);
